76969d3936678553f6fab9067b1dd91a009be3c1
Vikash <Vikashkm84@gmail.com>
2022-12-02 07:44:00 +0530

    commit
    done

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+const { errorHandler } = require("./backend/middlewares/errorHandler");
+const { connectDB } = require("./backend/configs/mongodb");
+const authRouter = require("./backend/routes/v1/auth.route");
+const userRouter = require("./backend/routes/v1/user.route");
+const categoryRouter = require("./backend/routes/v1/category.route");
+const sizeRouter = require("./backend/routes/v1/size.route");
+const productRouter = require("./backend/routes/v1/product.route");
+const orderRouter = require("./backend/routes/v1/order.route");
+const paymentRouter = require("./backend/routes/v1/payment.route");
+const uploadRouter = require("./backend/routes/v1/upload.route");
+
+const app = express();
+connectDB();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Routes
+app.use(
+  "/api/v1",
+  authRouter,
+  userRouter,
+  categoryRouter,
+  sizeRouter,
+  productRouter,
+  orderRouter,
+  paymentRouter,
+  uploadRouter
+);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// Unhandled route
+app.all("*", (req, res, next) => {
+  const err = new Error("The route can not be found");
+  err.statusCode = 404;
+  next(err);
+});
+
+// Error handle
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port}`);
+});
