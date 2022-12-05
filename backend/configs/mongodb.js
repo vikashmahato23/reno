9d5b68630c8b9e478e399a9d71d670451854cda6
Vikash <Vikashkm84@gmail.com>
2022-12-05 18:51:38 +0530

    j
    kk

--- backend/configs/mongodb.js
+++ backend/configs/mongodb.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose')
 
 const connectDB = async (req, res) => {
 	try {
-		const conn = mongoose.connect(process.env.MONGO_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
+		const conn = mongoose.connect(
+      "mongodb+srv://clipper:J123456789m@cluster0.xlvf7g7.mongodb.net/clipper?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-		console.log('Connected to MongoDB')
+		// console.log('Connected to MongoDB')
 	} catch (error) {
 		console.log('Error: ', error)
 	}
